09c5d0918c9b5c9afcd81a94c34cb44a2e535334
Dzmitry Kalianchuk <koirn.dev@gmail.com>
2024-06-16 10:46:36 +0200

    Updated kotlin to 2.0.0

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # MemoTv
 First experiment with Android TV
+
+Memo game implemented with Jetpack Compose for Android TV
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
@@ -38,14 +39,15 @@ android {
     buildFeatures {
         compose = true
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
-    }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+}
+
+composeCompiler {
+    enableStrongSkippingMode = true
 }
 
 dependencies {
